288075c9ac5b2100fc129879f062c02ecb1a5f37
apcom52 <apcom@live.ru>
2016-03-21 15:10:23 +0300

    2.3.0.102
    + Добавлены Routers для Timetable
    * Добавлен .gitignore

--- media/static/js/more_js/timetable.js
+++ media/static/js/more_js/timetable.js
@@ -1,3 +1,49 @@
 $(function() {
-	
+	var targetDay = new Date();
+	var targetWeekDay = new Date();
+	var targetMonthDay = new Date();
+
+	var Router = Backbone.Router.extend({
+		routes: {
+			"day/:date": "getDay",
+			"week/:date": "getWeek",
+			"month/:date": "getMonth",
+			"*actions": "default",
+		}
+	});
+
+	var router = new Router();
+
+	router.on('route:getDay', function(date) {
+		console.log('showDay');
+	});
+
+	router.on('route:getWeek', function(date) {
+		console.log('showWeek');
+	});
+
+	router.on('route:getMonth', function(date) {
+		console.log('showMonth');
+	});
+
+	router.on('route:default', function() {
+		window.location.hash = '#day/' + getParseDate(targetDay);
+	});
+
+	Backbone.history.start();
+
+	function getParseDate(date) {
+		var result = "";
+		var day = date.getDate();
+		var month = date.getMonth() + 1;
+		console.log(day);
+
+		if (day < 10) result += "0";
+		result += day;
+
+		if (month < 10) result += "0";
+		result += month;
+
+		return result;
+	}
 });
