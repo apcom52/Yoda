fced2e5f57570d7a3cf9227a519881fe773dd2fc
Alexander Perevezentsev <apcom@live.ru>
2016-06-29 10:10:56 +0300

    + Алгоритм генерации карты основывается теперь не только на рандоме, но и на соседних клетках + На клетках отображаются показатели, которые даются за обработку данных клеток + При наведении клетка затемняется

--- media/static/js/game/map.js
+++ media/static/js/game/map.js
@@ -50,27 +50,170 @@ Map.prototype.generate = function() {
 	var cells = this.cells;
 	for (var i = 0; i < target.y_size; i++) {
 		for (var j = 0; j < target.x_size; j++) {
-			var type = target.getRandomCell();
+			/* Генерация карты на основе окружающих клеток */
+			var type = "";
+
+			/* Если клетка первая, то генерируем случайную */
+			if (i == 0 && j == 0) {
+				type = target.getRandomCell();				
+			} else {
+				/* Если клетка в первой строке:
+				- 15% - слева
+				*/
+				if (i == 0) {
+					var rnd = Math.random();
+					if (rnd <= 0.15) type = cells[i][j - 1].type;
+					else type = target.getRandomCell();
+				} else {
+					/* Если клетка в других строках, то учиваем и левую, левую верхнюю, верхнюю и правую клетку 
+					ГОВНОКОД
+					*/
+					if (j == 0) {
+						if (cells[i-1][j].type == cells[i-1][j+1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						} else {
+							var rnd = Math.random();
+							if (rnd <= 0.15) type = cells[i-1][j].type;
+							else if (rnd > 0.15 && rnd <= 0.3) type = cells[i-1][j+1].type;
+							else type = target.getRandomCell();
+						}
+					} else if (j == target.x_size - 1) {
+						if (cells[i][j-1].type == cells[i-1][j-1].type == cells[i-1][j].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.45) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i][j-1].type == cells[i-1][j-1].type) != cells[i-1][j].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i][j-1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						} else if (cells[i][j-1].type != (cells[i-1][j-1].type == cells[i-1][j].type)) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j-1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i][j-1].type;
+							else type = target.getRandomCell();
+						} else if (cells[i-1][j-1].type != (cells[i][j-1].type == cells[i-1][j].type)) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i-1][j-1].type;
+							else type = target.getRandomCell();
+						} else {
+							var rnd = Math.random();
+							if (rnd <= 0.15) type = cells[i][j-1].type;
+							else if (rnd > 0.15 && rnd <= 0.3) type = cells[i-1][j-1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						}
+					} else {
+						if (cells[i][j-1].type == cells[i-1][j-1].type == cells[i-1][j].type == cells[i-1][j+1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.6) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i-1][j-1].type == cells[i][j-1].type == cells[i-1][j].type) != cells[i-1][j+1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.45) type = cells[i-1][j].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j+1].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i][j-1].type == cells[i-1][j+1].type == cells[i-1][j].type) != cells[i-1][j-1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.45) type = cells[i-1][j].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j-1].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i-1][j-1].type == cells[i-1][j+1].type == cells[i-1][j].type) != cells[i][j-1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.45) type = cells[i-1][j].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i][j-1].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i-1][j-1].type == cells[i-1][j+1].type == cells[i][j-1].type) != cells[i-1][j].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.45) type = cells[i-1][j-1].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i-1][j].type == cells[i-1][j+1].type) != cells[i][j-1].type != cells[i-1][j-1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j+1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i][j-1].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j-1].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i][j-1].type == cells[i-1][j-1].type) != cells[i-1][j].type != cells[i-1][j+1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j-1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i-1][j].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j+1].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i-1][j-1].type == cells[i-1][j].type) != cells[i][j-1].type != cells[i-1][j+1].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j-1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i][j-1].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j+1].type;
+							else type = target.getRandomCell();
+						} else if ((cells[i][j-1].type == cells[i-1][j+1].type) != cells[i-1][j-1].type != cells[i-1][j].type) {
+							var rnd = Math.random();
+							if (rnd <= 0.3) type = cells[i-1][j+1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i-1][j-1].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j].type;
+							else type = target.getRandomCell();
+						} else {
+							var rnd = Math.random();
+							if (rnd <= 0.15) type = cells[i][j-1].type;
+							else if (rnd > 0.15 && rnd <= 0.3) type = cells[i-1][j-1].type;
+							else if (rnd > 0.3 && rnd <= 0.45) type = cells[i-1][j].type;
+							else if (rnd > 0.45 && rnd <= 0.6) type = cells[i-1][j+1].type;
+							else type = target.getRandomCell();
+						}
+					}
+				}
+			}
+
+			var food = 0;
+			var production = 0;
+			var culture = 0;
+			var faith = 0;
+			var science = 0;
+			var tourism = 0;
+			var happiness = 0;
+			var gold = 0;
+
+			if (type == target.PLAIN) {
+				food = 1;
+				console.log("PLAIN");
+			}
+
+			if (type == target.SAND) {
+				faith = 1;
+			}
+
+			if (type == target.SEA) {
+				food = 1;
+			}
+
+			if (type == target.MOUNTAIN) {
+				production = 1;
+				science = 1;
+			}
+
 			cells[i][j] = {
 				type: type,
 				sprite: type,
 				resource: "",
 				building: "",
 				values: {
-					food: 0,
-					production: 0,
-					culture: 0,
-					faith: 0,
-					science: 0,
-					tourism: 0,
-					happiness: 0,
-					gold: 0,
+					food: food,
+					production: production,
+					culture: culture,
+					faith: faith,
+					science: science,
+					tourism: tourism,
+					happiness: happiness,
+					gold: gold,
 				},
 				position: {
 					x: j,
 					y: i,
 				}
-			};
+			};	
 		}
 	}
 
@@ -143,8 +286,6 @@ Map.prototype.generate = function() {
 					}
 				}	
 
-				console.log(i + ";" + j + " => " + str);
-
 				switch(str) {
 					case "11111111":
 						current.sprite = "seaEWNS";
@@ -182,7 +323,15 @@ Map.prototype.generate = function() {
 Map.prototype.getRandomCell = function() {
 	var rnd = Math.random();
 	if (rnd <= 0.5) return this.PLAIN;
-	else if (rnd > 0.5 && rnd <= 0.72) return this.SAND;
-	else if (rnd > 0.72 && rnd <= 0.93) return this.SEA;
+	else if (rnd > 0.5 && rnd <= 0.7) return this.SAND;
+	else if (rnd > 0.7 && rnd <= 0.93) return this.SEA;
 	else return this.MOUNTAIN; 
+}
+
+Map.prototype.onInputOver = function(item) {
+	console.log(this.position);
+}
+
+Map.prototype.onInputOut = function(item) {
+	console.log("out");
 }
--- media/static/js/game/render.js
+++ media/static/js/game/render.js
@@ -11,7 +11,34 @@ Render.prototype.draw = function(map = undefined) {
 
 	for (var i = 0; i < map.y_size; i++) {
 		for (var j = 0; j < map.x_size; j++) {
-			game.add.sprite(64 * j, 64 * i, map.cells[i][j].sprite);
+			var cell = map.cells[i][j];
+			var current = game.add.sprite(64 * j, 64 * i, cell.sprite);
+			current.inputEnabled = true;
+			current.events.onInputOver.add(over, this);
+			current.events.onInputOut.add(out, this);
+
+			var str = "";
+			if (cell.values.food) str += "🍎 " + cell.values.food + "\n";
+			if (cell.values.production) str += "🔨 " + cell.values.production + "\n";
+			if (cell.values.culture) str += "✒ " + cell.values.culture + "\n";
+			if (cell.values.gold) str += "○ " + cell.values.gold + "\n";
+			if (cell.values.faith) str += "🐦 " + cell.values.faith + "\n";
+			if (cell.values.science) str += "👓 " + cell.values.science + "\n";
+
+			if (str) {
+				game.add.text(64*j, 64*i, str, {
+					font: "bold 14px Arial",
+					fill: "#000",					
+				})
+			}
 		}
 	}
+}
+
+function over(item) {
+	item.alpha = 0.5; 
+}
+
+function out(item) {
+	item.alpha = 1; 
 }
